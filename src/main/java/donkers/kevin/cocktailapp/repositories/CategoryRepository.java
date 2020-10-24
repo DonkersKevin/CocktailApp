c43da97aae7338d4157aae3f8dac6deb53fe29da
DonkersKevin <Donkers.Kevin86@gmail.com>
2020-10-24 19:25:18 +0200

    Added custom finders

--- src/main/java/donkers/kevin/cocktailapp/repositories/CategoryRepository.java
+++ src/main/java/donkers/kevin/cocktailapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package donkers.kevin.cocktailapp.repositories;
 import donkers.kevin.cocktailapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/donkers/kevin/cocktailapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/donkers/kevin/cocktailapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package donkers.kevin.cocktailapp.repositories;
 import donkers.kevin.cocktailapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription (String description);
 }
